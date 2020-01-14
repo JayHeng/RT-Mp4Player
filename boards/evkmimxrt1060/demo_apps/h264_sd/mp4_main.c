0a5994bd24794333bc08213d3993842aa4e97a1c
Jay Heng <jie.heng@nxp.com>
2020-01-14 13:39:14 +0800

    [ZIP] Cleanup initial ffmpeg encode pure code
    - Refined coding style
    
    Signed-off-by: Jay Heng <jie.heng@nxp.com>

--- boards/evkmimxrt1060/demo_apps/h264_sd/mp4_main.c
+++ boards/evkmimxrt1060/demo_apps/h264_sd/mp4_main.c
@@ -825,123 +825,133 @@ static void h264_file_encode(const char *vinfilename, const char *voutfilename)
 static void h264_file_encode_pure(const char *vinfilename, const char *voutfilename)
 {
     AVCodec *pCodec;
-    AVCodecContext *pCodecCtx= NULL;
+    AVCodecContext *pCodecCtx = NULL;
     int i, ret, got_output;
     FILE *fp_in;
-	FILE *fp_out;
+    FILE *fp_out;
     AVFrame *pFrame;
     AVPacket pkt;
-	int y_size;
-	int framecnt=0;
+    int y_size;
+    int framecnt = 0;
 
-	char filename_in[]="../ds_480x272.yuv";
+    char filename_in[] = "../ds_480x272.yuv";
+    AVCodecID codec_id = AV_CODEC_ID_H264;
+    char filename_out[] = "ds.h264";
 
-#if TEST_HEVC
-	AVCodecID codec_id=AV_CODEC_ID_HEVC;
-	char filename_out[]="ds.hevc";
-#else
-	AVCodecID codec_id=AV_CODEC_ID_H264;
-	char filename_out[]="ds.h264";
-#endif
+    int in_w = 480,in_h = 272;
+    int framenum = 100;	
 
-
-	int in_w=480,in_h=272;	
-	int framenum=100;	
-
-	avcodec_register_all();
+    avcodec_register_all();
 
     pCodec = avcodec_find_encoder(codec_id);
-    if (!pCodec) {
+    if (!pCodec) 
+    {
         printf("Codec not found\n");
-        return -1;
+        return;
     }
     pCodecCtx = avcodec_alloc_context3(pCodec);
-    if (!pCodecCtx) {
+    if (!pCodecCtx)
+    {
         printf("Could not allocate video codec context\n");
-        return -1;
+        return;
     }
     pCodecCtx->bit_rate = 400000;
     pCodecCtx->width = in_w;
     pCodecCtx->height = in_h;
     pCodecCtx->time_base.num=1;
-	pCodecCtx->time_base.den=25;
+    pCodecCtx->time_base.den=25;
     pCodecCtx->gop_size = 10;
     pCodecCtx->max_b_frames = 1;
     pCodecCtx->pix_fmt = AV_PIX_FMT_YUV420P;
 
     if (codec_id == AV_CODEC_ID_H264)
+    {
         av_opt_set(pCodecCtx->priv_data, "preset", "slow", 0);
+    }
  
-    if (avcodec_open2(pCodecCtx, pCodec, NULL) < 0) {
+    if (avcodec_open2(pCodecCtx, pCodec, NULL) < 0)
+    {
         printf("Could not open codec\n");
-        return -1;
+        return;
     }
     
     pFrame = av_frame_alloc();
-    if (!pFrame) {
+    if (!pFrame)
+    {
         printf("Could not allocate video frame\n");
-        return -1;
+        return;
     }
     pFrame->format = pCodecCtx->pix_fmt;
     pFrame->width  = pCodecCtx->width;
     pFrame->height = pCodecCtx->height;
 
-    ret = av_image_alloc(pFrame->data, pFrame->linesize, pCodecCtx->width, pCodecCtx->height,
-                         pCodecCtx->pix_fmt, 16);
-    if (ret < 0) {
+    ret = av_image_alloc(pFrame->data, pFrame->linesize, pCodecCtx->width, pCodecCtx->height, pCodecCtx->pix_fmt, 16);
+    if (ret < 0)
+    {
         printf("Could not allocate raw picture buffer\n");
-        return -1;
+        return;
     }
-	//Input raw data
-	fp_in = fopen(filename_in, "rb");
-	if (!fp_in) {
-		printf("Could not open %s\n", filename_in);
-		return -1;
-	}
-	//Output bitstream
-	fp_out = fopen(filename_out, "wb");
-	if (!fp_out) {
-		printf("Could not open %s\n", filename_out);
-		return -1;
-	}
+    //Input raw data
+    fp_in = fopen(filename_in, "rb");
+    if (!fp_in)
+    {
+        printf("Could not open %s\n", filename_in);
+        return;
+    }
+    //Output bitstream
+    fp_out = fopen(filename_out, "wb");
+    if (!fp_out)
+    {
+        printf("Could not open %s\n", filename_out);
+        return;
+    }
 
-	y_size = pCodecCtx->width * pCodecCtx->height;
+    y_size = pCodecCtx->width * pCodecCtx->height;
     //Encode
-    for (i = 0; i < framenum; i++) {
+    for (i = 0; i < framenum; i++)
+    {
         av_init_packet(&pkt);
         pkt.data = NULL;    // packet data will be allocated by the encoder
         pkt.size = 0;
-		//Read raw YUV data
-		if (fread(pFrame->data[0],1,y_size,fp_in)<= 0||		// Y
-			fread(pFrame->data[1],1,y_size/4,fp_in)<= 0||	// U
-			fread(pFrame->data[2],1,y_size/4,fp_in)<= 0){	// V
-			return -1;
-		}else if(feof(fp_in)){
-			break;
-		}
+        //Read raw YUV data
+        if (fread(pFrame->data[0],1,y_size,fp_in)<= 0 ||    // Y
+            fread(pFrame->data[1],1,y_size/4,fp_in)<= 0 ||  // U
+            fread(pFrame->data[2],1,y_size/4,fp_in)<= 0)    // V
+        {
+            return;
+        }
+        else if (feof(fp_in))
+        {
+            break;
+        }
 
         pFrame->pts = i;
         /* encode the image */
         ret = avcodec_encode_video2(pCodecCtx, &pkt, pFrame, &got_output);
-        if (ret < 0) {
+        if (ret < 0)
+        {
             printf("Error encoding frame\n");
-            return -1;
+            return;
         }
-        if (got_output) {
+        if (got_output)
+        {
             printf("Succeed to encode frame: %5d\tsize:%5d\n",framecnt,pkt.size);
-			framecnt++;
+            framecnt++;
             fwrite(pkt.data, 1, pkt.size, fp_out);
             av_free_packet(&pkt);
         }
     }
     //Flush Encoder
-    for (got_output = 1; got_output; i++) {
+    for (got_output = 1; got_output; i++)
+    {
         ret = avcodec_encode_video2(pCodecCtx, &pkt, NULL, &got_output);
-        if (ret < 0) {
+        if (ret < 0)
+        {
             printf("Error encoding frame\n");
-            return -1;
+            return;
         }
-        if (got_output) {
+        if (got_output)
+        {
             printf("Flush Encoder: Succeed to encode 1 frame!\tsize:%5d\n",pkt.size);
             fwrite(pkt.data, 1, pkt.size, fp_out);
             av_free_packet(&pkt);
@@ -953,8 +963,6 @@ static void h264_file_encode_pure(const char *vinfilename, const char *voutfilen
     av_free(pCodecCtx);
     av_freep(&pFrame->data[0]);
     av_frame_free(&pFrame);
-
-	return 0;
 }
 
 static void h264_camera_encode(const char *voutfilename)
